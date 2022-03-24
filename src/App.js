52e2ea36c181a29c4afb8bb02273e86d8a50de5d
melhamdaoui811 <melhamdaoui811@gmail.com>
2022-03-24 22:16:12 +0100

    add material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import { Button } from "@material-ui/core";
+import {Person} from "@material-ui/icons"
+import { makeStyles } from "@material-ui/core";
+const useStyles = makeStyles({
+  btn:{
+    fontSize:12,
+    color:"red",
+  }
+})
 function App() {
+ const classes=useStyles();
+ console.log(classes.btn)
   return (
-    <div>Hello world</div>
-  )
+    
+    <div>
+      
+      <Button
+      
+      className={classes.btn}
+      startIcon={<Person />}
+      variant='contained'
+       size='large'
+       color="Primary"
+      
+       
+       >
+        Material UI
+      </Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
